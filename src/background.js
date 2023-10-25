1c71523d3f64591ba8734bc2ea527f1420641f3a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-10-25 14:35:35 +0800

    :bug: 修正Linux打包异常 此异常是在 Linux 下默认使用 build/icon.ico 资源时未通过验证
      ⨯ unknown output format set
    github.com/develar/app-builder/pkg/icons.convertSingleFile
    	/Volumes/data/Documents/app-builder/pkg/icons/icon-converter.go:305
    github.com/develar/app-builder/pkg/icons.doConvertIcon
    	/Volumes/data/Documents/app-builder/pkg/icons/icon-converter.go:247
    github.com/develar/app-builder/pkg/icons.ConvertIcon
    	/Volumes/data/Documents/app-builder/pkg/icons/icon-converter.go:56
    github.com/develar/app-builder/pkg/icons.ConfigureCommand.func1
    	/Volumes/data/Documents/app-builder/pkg/icons/icon-converter.go:33
    github.com/alecthomas/kingpin.(*actionMixin).applyActions
    	/Volumes/data/go/pkg/mod/github.com/alecthomas/kingpin@v2.2.6+incompatible/actions.go:28
    github.com/alecthomas/kingpin.(*Application).applyActions
    	/Volumes/data/go/pkg/mod/github.com/alecthomas/kingpin@v2.2.6+incompatible/app.go:557
    github.com/alecthomas/kingpin.(*Application).execute
    	/Volumes/data/go/pkg/mod/github.com/alecthomas/kingpin@v2.2.6+incompatible/app.go:390
    github.com/alecthomas/kingpin.(*Application).Parse
    	/Volumes/data/go/pkg/mod/github.com/alecthomas/kingpin@v2.2.6+incompatible/app.go:222
    main.main
    	/Volumes/data/Documents/app-builder/main.go:90
    runtime.main
    	/usr/local/Cellar/go/1.17/libexec/src/runtime/proc.go:255
    runtime.goexit
    	/usr/local/Cellar/go/1.17/libexec/src/runtime/asm_amd64.s:1581

--- src/background.js
+++ src/background.js
@@ -65,7 +65,7 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: 'build/icon.ico',
+    icon: 'build/icon-256x256.ico',
     webPreferences: {
       // webSecurity：默认值：true，设置为 false 则会允许所有跨域，不支持热加载
       preload: path.join(__dirname, 'preload.js')
